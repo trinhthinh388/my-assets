0b4ceda03e850773d92a7fa864c6a873e171c0b1
thinh <thinh@thinhtrinh.local>
2022-02-21 19:28:27 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Layout } from 'antd';
+import '../styles/global.css';
+import 'antd/dist/antd.css';
+import '@fontsource/roboto';
+
+const { Content, Footer } = Layout;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Content className="px-5">
+        <Component {...pageProps} />
+      </Content>
+      <Footer>Meta © 2022</Footer>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
